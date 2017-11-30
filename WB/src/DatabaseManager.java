8f02e2ee5da76e02fe58e56c9e98dd03bf4d93fc
colerogers <colerogers@umail.ucsb.edu>
2017-11-30 13:34:01 -0800

    updated how stock account is set up on mysql

--- WB/src/DatabaseManager.java
+++ WB/src/DatabaseManager.java
@@ -149,7 +149,7 @@ public class DatabaseManager {
 			return 1;
 		}
 
-		String s = String.format("INSERT INTO StockAccount (stock_symbol, c_username, shares, stock_price) VALUES ('%s', '%s', %f, %f);", stock_symbol, c_username, shares, stock_price);
+		String s = String.format("INSERT INTO StockAccount (c_username, shares, stock_price, stock_symbol) VALUES ('%s', %f, %f, '%s');", c_username, shares, stock_price, stock_symbol);
 		// add the SA to the DB
 		return updateDB(s);
 	}
